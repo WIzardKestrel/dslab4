5dec5b642c48fc81afd908105af8887ccfa23749
wk85 <41642920+WIzardKestrel@users.noreply.github.com>
2021-11-08 17:38:09 +0200

    Update function.h

--- function.h
+++ function.h
@@ -14,22 +14,27 @@ typedef struct nodeStudent
 }snode;
 struct nodeClass *createClass(struct nodeClass * head);
 struct nodeStudent * insort(struct nodeStudent * head, int id, int grade);
-void printA(struct nodeClass * head);
+//void printA(struct nodeClass * head);
 void insert(struct nodeClass ** head, int id, int grade) {
+
     if (*head == NULL){
         *head = createClass(*head);
+        cnode *t = *head;
+        int cid = (int)(id/10000) - 6;
+        for (int i = 0; i < cid; ++i) {
+            t = t -> next;
+        }
+        t -> studentPtr = insort(t -> studentPtr, id, grade);
         //printA(*head);
     }else{
         cnode * temp = *head;
         snode * sTemp = NULL;
         int stdId = id;
-        int classID = (int)(stdId / 10000) - 6;
+        int classID = (int)(stdId / 10000) - 6 ;
         for (int i = 0; i < classID; ++i) {
             temp = temp -> next;
         }
         temp -> studentPtr = insort(temp -> studentPtr, id, grade);
-        if(temp -> studentPtr == NULL)
-            printf("its nul\n");
     }
 }
 snode * insort(snode * head, int id, int grade){
@@ -44,7 +49,7 @@ snode * insort(snode * head, int id, int grade){
         return head;
     }
     while(temp -> next != NULL && temp -> next -> midterm > grade){
-        printf("passing\n");
+        //printf("passing\n");
         temp = temp->next;
     }
     ptr -> next = temp -> next;
@@ -79,18 +84,40 @@ struct nodeClass* createClass(struct nodeClass *head)
     return head;
 }
 
-void printA(struct nodeClass *head){
-    printf("id\tgrade\n");
+void computeClassAverage(cnode * head){
+    cnode * ptr = head;
+    snode *temp = head -> studentPtr;
+    double sum = 0, count = 0;
+    while(1){
+        sum = 0, count = 0;
+        while(temp != NULL){
+            sum += temp -> midterm;
+            //printf("sum is: %.2f\t", sum);
+            count++;
+            temp = temp -> next;
+        }
+        //printf("average is: %.2f\n", sum/count);
+        if(count != 0)
+            ptr -> classMidtermAverage = (sum / count);
+        else
+            ptr -> classMidtermAverage = 0;
+        ptr = ptr -> next;
+        if(ptr == NULL)
+            break;
+        temp = ptr -> studentPtr;
+    }
+}
+
+void printAll(struct nodeClass *head){
     snode * temp;
     while(head != NULL){
+        printf("%d %.2f\n", head -> classID, head -> classMidtermAverage);
         temp = head -> studentPtr;
-
-        if(temp == NULL)
-            printf("null, won't print\n");
         while(temp != NULL){
-            printf("%d\t%d\n", temp -> studentID, temp -> midterm);
+            printf("%d %d\n", temp -> studentID, temp -> midterm);
             temp = temp -> next;
-        }printf("classid: %d^^\n", head -> classID);
+        }
+        //printf("averge for class %d: %.2f\n", head -> classID, head -> classMidtermAverage);
         head = head ->next;
     }
 
